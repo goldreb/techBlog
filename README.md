4ab0aeebee4fba5208069ffa860bf84c809e7b37
Goldiluck Rebollido <gorebollido@gmail.com>
2021-05-04 22:21:02 -0700

    added files

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
 # techBlog
+
+## Table of Contents
+
+- [Description](#Description)
+- [License](#License-Badge)
+- [Packages](#Packages)
+- [Technologies](#Technologies)
+- [Mock-Ups](#Mock-Ups)
+
+## Description
+
+This is a homework for University of Oregon coding bootcamp.
+
+## License-Badge
+
+![](https://img.shields.io/badge/license-MIT-blue)
+
+## Packages
+
+- npm packages
+
+## Technologies
+
+- Node.js
+- NPM
+- MySql
+- Heroku
+- Express
+- handlebars
+
+## Mock-Ups
+
+MySql Database is seeded
+![Database seeded](images/userseeded.JPG)
+
+MySql Cookies stored
+![Database cookies](images/database-cookies.JPG)
+
+npm start to run the server
+![Server Start](images/serverListening.JPG)
+
+localhost login
+![Login](images/login.JPG)
+
+localhost dashboard
+![Dashboard](images/dashboard.JPG)
+
+localhost blog seeds
+![blog test](images/blogTest.JPG)
--- config/connection.js
+++ config/connection.js
@@ -2,18 +2,26 @@ const Sequelize = require('sequelize');
 
 require('dotenv').config();
 
-
-//create connection to mysql database
-const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
+//connection to mysql and heroku
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
       host: 'localhost',
       dialect: 'mysql',
-      port: 3306,
-    }
-  );
+      port: 3306
+    });
+
+//create connection to mysql database
+// const sequelize = new Sequelize(
+//     process.env.DB_NAME,
+//     process.env.DB_USER,
+//     process.env.DB_PASSWORD,
+//     {
+//       host: 'localhost',
+//       dialect: 'mysql',
+//       port: 3306,
+//     }
+//   );
   
   module.exports = sequelize;
   
